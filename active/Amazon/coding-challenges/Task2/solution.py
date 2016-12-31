9bb5f4105e975a708b612c85f59e7bb808381e7f
Tim Condit <tcondit@gmail.com>
2016-12-30 21:50:25 -0800

    wip

---
+++ active/Amazon/coding-challenges/Task2/solution.py
@@ -0,0 +1,17 @@
+# you can write to stdout for debugging purposes, e.g.
+# print "this is a debug message"
+
+def solution(A, B):
+    # A integer in range [0..100,000,000]
+    # B integer in range [0..100,000,000]
+    # zip them
+    # return -1 for results > 100,000,000
+
+    # get A, convert to list, get length
+    # get B, convert to list, get length
+
+    _A = list(str(A))
+    _B = list(str(B))
+
+    map(None, _A, _B)
+
