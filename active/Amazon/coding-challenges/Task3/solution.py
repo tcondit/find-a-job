7179bddaa9c2f616cb54ec7b5135a5b7842b3faa
Tim Condit <tcondit@gmail.com>
2016-12-30 22:56:02 -0800

    wip

--- active/Amazon/coding-challenges/Task3/solution.py
+++ active/Amazon/coding-challenges/Task3/solution.py
@@ -1,8 +1,4 @@
 def solution(A):
-#    unsorted_A = A
-#    sorted_A = sorted(A)
-
-#    return A == sorted(A)
 
     # find first number that drops after increasing or stay same (e.g., [1, 5, 3, 3, 7] -> 3)
     # "back up" and pick one just before (e.g., 5)
@@ -12,6 +8,9 @@ def solution(A):
     #
     # True if == sorted, else False
 
+    if A == sorted(A):
+        return True
+
     max = 0
     _I = 0
     _J = 0
@@ -20,25 +19,22 @@ def solution(A):
         if m >= max:
             max = m
         else:
-#            print 'breaking', m 
             _I = A.index(m)
-            break # found first local max
-#    print '1/one step beyond:', _I
+            break
 
-    # this is I
     I = A[_I-1]
+    print 'I:', I
 
-    for m in range(_I, len(A)):
-        print 'A[m]:', A[m]
-        if A[m] < I:
-#            print 'continuing'
-            continue
-        # found second local max
-        _J = A.index(A[m])
-        print '2/one step beyond:', _J
+    max2 = A[_I]
+    print 'A[_I]:', A[_I]
 
-    # this is J
+    for m in range(_I, len(A)-1):
+        if m >= max2:
+            max2 = m
+        else:
+            _J = A.index(m)
+            break
+
     J = A[_J-1]
-
-    print 'A[m]:', A[m]
     print 'J:', J
+
